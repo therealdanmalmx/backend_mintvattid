0590df385a867d2343c36546775c3266896e6be8
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-12-06 20:59:25 +0100

    Show apartment number and full name on usersperproperty

--- Dtos/User/GetUserDto.cs
+++ Dtos/User/GetUserDto.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace backend.Dtos.UserDto
 {
     public class GetUserDto
     {
-        public Guid Id { get; set; }
-        public string UserName { get; set; } = string.Empty;
         public string ApartmentNumber { get; set; } = string.Empty;
 
+        [JsonIgnore]
+        public string FirstName { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public string LastName { get; set; } = string.Empty;
+
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
