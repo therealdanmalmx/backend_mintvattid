21a4cd22b8643d66464eb9ba6f5b0115a3e894e2
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-12-06 13:34:10 +0100

    Update swagger for production use as well

--- Program.cs
+++ Program.cs
@@ -80,7 +80,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
